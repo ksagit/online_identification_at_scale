139776fa1b6482dd77edb9948de5172f73307f21
ksagit <kylesargent@college.harvard.edu>
2018-12-17 07:34:58 +0000

    final commit

--- load_and_transform.py
+++ load_and_transform.py
@@ -19,12 +19,23 @@ def transform_image(pil_image):
     img = img.transpose(0,1)
     return img
 
-def get_image_list():
-    images = []
-    for person in os.listdir(DATASET_PATH):
-        for image in os.listdir(os.path.join(DATASET_PATH, person)):
-            images += [[os.path.join(DATASET_PATH, person, image), person]]
-    return np.array(images)
+def pairwise_distances(x,y):
+    x_norms = torch.sum(x*x, dim=1).unsqueeze(1)
+    y_norms = torch.sum(y*y, dim=1).unsqueeze(0)    
+    return x_norms - 2*torch.mm(x, y.transpose(0, 1)) + y_norms
+
+def get_image_list(n_people=len(os.listdir(DATASET_PATH)), n_images_per_person=1000):
+    all_images = []
+    for person in np.random.choice(os.listdir(DATASET_PATH), n_people, replace=False):
+        image_files = os.listdir(os.path.join(DATASET_PATH, person))
+        if len(image_files) > n_images_per_person:
+            person_images = np.random.choice(image_files, n_images_per_person, replace=False)
+        else:
+            person_images = image_files
+            
+        for image in person_images:
+            all_images += [[os.path.join(DATASET_PATH, person, image), person]]
+    return np.array(all_images)
 
 def load_image_batch(image_filenames):
     batch_images = map(lambda filename: PIL.Image.open(filename), image_filenames)
@@ -33,3 +44,4 @@ def load_image_batch(image_filenames):
     batch_images = torch.cat(batch_images, 0)
     batch_images = batch_images.cuda()
     return batch_images
+
